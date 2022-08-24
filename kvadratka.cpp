97602bb36adf0d7ba3f9ca03c36f20b5ff1657f3
true-dude <79796550+true-dude@users.noreply.github.com>
2022-08-24 19:22:08 +0300

    уже лучше

--- kvadratka.cpp
+++ kvadratka.cpp
@@ -1,9 +1,20 @@
+/*!
+Tut text
+
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
 #include <ctype.h>
 #include <string.h>
+#include <ctype.h>
+#include <string.h>
+
+
+#define min(a, b) ((a) < (b)) ? (a) : (b)
+#define max(a, b) ((a) > (b)) ? (a) : (b)
 
 const double EPS = 1e-8;
 const int MAXSIZE = 1000;
@@ -14,21 +25,35 @@ enum Flags {
     ONE_ROOT,
     TWO_ROOTS,
     INF_SOL,
+    WRONG = 0,
+    CORRECT= 1
 };
 
 int isNull(double);
-int solLineyka(double, double, double*);
-int solKvadratka(double, double, double, double*, double*);
+int solveLineyka(double, double, double*);
+int solveKvadratka(double, double, double, double*, double*);
 void pretyPrintRoots(int, double, double);
-void readAllChars();
+void clearBuffer();
 int find_quit();
+void test_SK();
+void test_case(double, double, double, int, double, double);
+int eqd(double, double);
+void userInput();
 
+//!---------------------------------------------
+//!@param [in] a number to compare against zero
+//!---------------------------------------------
 int isNull(double a)
 {
-    return abs(a) < EPS;
+    return fabs(a) < EPS;
 }
 
-int solLineyka(double b, double c, double* px)
+//!--------------------------------------------
+//!@param [in]  a  a-coefficient
+//!@param [in]  b  b-coefficient
+//!@param [out] px pointer to the root
+//!--------------------------------------------
+int solveLineyka(double b, double c, double* px)
 {
     assert(std::isfinite(b));
     assert(std::isfinite(c));
@@ -41,80 +66,98 @@ int solLineyka(double b, double c, double* px)
         }
         else
         {
-            return 0;
+            return NULL_ROOTS;
         }
     }
     *px = -c / b;
     return 1;
 }
 
-int solKvadratka(double a, double b, double c, double* px1, double* px2)
+//!--------------------------------------------
+//!
+//!@param [in]  a a-coefficient
+//!@param [in]  b b-coefficient
+//!@param [in]  c c-coefficient
+//!@param [out] px1 pointer to the first root
+//!@param [out] px2 pointer to the second root
+//!--------------------------------------------
+int solveKvadratka(double a_coef, double b_coef, double c_coef, double* px1, double* px2)
 {
-    assert(std::isfinite(a));
-    assert(std::isfinite(b));
-    assert(std::isfinite(c));
+    assert(std::isfinite(a_coef));
+    assert(std::isfinite(b_coef));
+    assert(std::isfinite(c_coef));
     assert(px1 != NULL);
     assert(px2 != NULL);
     assert(px1 != px2);
-    if (isNull(a))
+    if (isNull(a_coef))
     {
-        if (isNull(b))
+        if (isNull(b_coef))
         {
-            if (isNull(c))
+            if (isNull(c_coef))
             {
                 return INF_SOL;
             }
             else
             {
-                return 0;
+                return NULL_ROOTS;
             }
         }
         else
         {
-            return solLineyka(b, c, px1);
+            return solveLineyka(b_coef, c_coef, px1);
         }
     }
-    double d = b * b - 4 * a * c;
-    if (isNull(d))
+    double D = b_coef * b_coef - 4 * a_coef * c_coef;
+    if (isNull(D))
     {
-        *px1 = -b / (2 * a);
+        *px1 = -b_coef / (2 * a_coef);
         return 1;
     }
-    if (d < 0)
+    if (D < 0)
     {
-        return 0;
+        return NULL_ROOTS;
     }
     else
     {
-        *px1 = (-b + sqrt(d)) / (2 * a);
-        *px2 = (-b - sqrt(d)) / (2 * a);
-        return 2;
+        double sqrtD = sqrt(D);
+        *px1 = (-b_coef + sqrtD) / (2 * a_coef);
+        *px2 = (-b_coef - sqrtD) / (2 * a_coef);
+        double tmp;
+        tmp  = min(*px1, *px2);
+        *px2 = max(*px1, *px2);
+        *px1 = tmp;
+        return TWO_ROOTS;
     }
 }
 
+//-----------------------------------
+//!@param [in] nRoots number of roots
+//!@param [in] x1     1-st root
+//!@param [in] x2     2-nd root
+//------------------------------------
 void pretyPrintRoots(int nRoots, double x1, double x2)
 {
     switch(nRoots)
         {
             case NULL_ROOTS:
-                printf("Уравнение не имеет корней\n");
+                printf("The equation has no roots\n");
                 break;
             case ONE_ROOT:
-                printf("Уравнение имеет один корень: x = %.2lf\n", x1);
+                printf("The equation has one root: x = %.2lf\n", x1);
                 break;
             case TWO_ROOTS:
-                printf("Уравнение имеет два корня: x1 = %.2lf, x2 = %.2lf\n", x1, x2);
+                printf("The equation has two roots: x1 = %.2lf, x2 = %.2lf\n", x1, x2);
                 break;
             case INF_SOL:
-                printf("Уравнение имеет бесконечно много корней\n");
+                printf("The equation has infinitely many roots\n");
                 break;
             default:
-                printf("Программа завершилась с ошибкой :(\n");
+                printf("The program ended with an error :(\n");
         }
 }
 
 
-void readAllChars()
+void clearBuffer()
 {
     int c;
     while ((c = getchar()) != EOF && c != '\n');
@@ -128,16 +171,63 @@ int find_quit()
 }
 
 
-int main()
+void test_SK()
 {
-    double a = NAN, b = NAN, c = NAN; //коэффициенты квадратного уравнения
-    double x1 = NAN, x2 = NAN;        //корни уравнения
-    int nRoots = 0;                   //количество корней уравнения
-    int nCoeficients = 0;             //количество считанных коэффициентов уравнения
-    printf("Для завершения программы введите quit\n");
+
+    test_case(0,    0,    0,    INF_SOL,  0, 0);
+    test_case(1,    2,   -3,  TWO_ROOTS, -3, 1);
+    test_case(5,    2,    8, NULL_ROOTS,  0, 0);
+    test_case(1,    2,    1,   ONE_ROOT, -1, 0);
+    test_case(1, 10.5, 13.5,  TWO_ROOTS, -9, -1.5);
+
+}
+
+
+void test_case(double a, double b, double c, int tnRoots, double tx1, double tx2)
+{
+    double x1 = NAN, x2 = NAN;
+    int nRoots = solveKvadratka(a, b, c, &x1, &x2);
+    int status = CORRECT;
+    if (nRoots != tnRoots)
+    {
+        status = WRONG;
+    }
+    switch (nRoots)
+    {
+        case ONE_ROOT:
+            status = (eqd(x1, tx1)) ? CORRECT : WRONG;
+            break;
+        case TWO_ROOTS:
+            status = (eqd(x1, tx1) && eqd(x2, tx2)) ? CORRECT : WRONG;
+            break;
+        default:
+            status = CORRECT;
+    }
+    if (status == WRONG)
+    {
+        printf("Failed:   nRoots = %d, x1 = %lg, x2 = %lg\n"
+               "Expected: nRoots = %d, x1 = %lg, x2 = %lg\n"
+             "With arguments a = %lg, b = %lg, c = %lg\n",
+             nRoots, x1, x2, tnRoots, tx1, tx2, a, b, c);
+    }
+}
+
+int eqd(double a, double b)
+{
+    return ((a < (b + EPS)) && (a > (b - EPS)));
+}
+
+
+void userInput()
+{
+   double a = NAN, b = NAN, c = NAN; //quadratic equation coefficients
+    double x1 = NAN, x2 = NAN;        //equation roots
+    int nRoots = 0;                   //number of roots of the equation
+    int nCoeficients = 0;             //number of equation coefficients read
+    printf("To end the program, enter quit\n");
     while (true)
     {
-        printf("Введите коэффициенты квадратного уравнения: ");
+        printf("Enter the coefficients of the quadratic equation: ");
         nCoeficients = scanf("%lg %lg %lg", &a, &b, &c);
         if (nCoeficients != 3)
         {
@@ -145,11 +235,18 @@ int main()
             {
                 break;
             }
-            readAllChars();
-            printf("Введите нормальные значения\n");
+            clearBuffer();
+            printf("Enter normal values\n"
+            "For example: 1.4 3 -5.4\n");
             continue;
         }
-        nRoots = solKvadratka(a, b, c, &x1, &x2);
+        nRoots = solveKvadratka(a, b, c, &x1, &x2);
         pretyPrintRoots(nRoots, x1, x2);
     }
+}
+
+
+int main()
+{
+    userInput();
 }
